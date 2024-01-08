c1588ebfef6af5d515e34ddf1f2bb47b063c3a73
georgeuwagbale <uwagbalegeorge@gmail.com>
2024-01-08 23:35:19 +0100

    Added Mini os and Apoowo projects

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -52,7 +52,7 @@ const Work = () => {
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/Apo-Owo">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
